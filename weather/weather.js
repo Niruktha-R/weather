58111dc39a2390988395ba16490ac74d9da2714e
Niruktha-R <120166619+Niruktha-R@users.noreply.github.com>
2024-03-08 21:16:57 +0530

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,30 @@
+var inputvalue = document.querySelector('#cityinput')
+var btn = document.querySelector('#add')
+var city = document.querySelector('#cityinput')
+var descip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik="ce29163cccdb8efbc7a7bd8d1d9c1508"
+function convertion(val) {
+    return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click', function()
+{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+    .then(res => res.json())
+
+    .then(data =>
+       {
+        var nameval = data['name']
+        var descrip = data['weather']['0']['description']
+        var temperature = data['main']['temp']
+        var wndspeed = data['wind']['speed']
+
+        city.innerHTML=`weather of <span>${nameval}<span>`
+        temp.innerHTML=`Temperature: <span>${ convertion(temperature)} C</span>`
+        description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+        wind.innerHTML = `Wind Speed: <span>${wndspeed} km/h<span>`
+       } )
+       .catch(err => alert('You entered Wrong city name'))
+})
